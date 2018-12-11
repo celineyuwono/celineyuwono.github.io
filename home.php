49e9c3f1670f0e902dddb286eaa7d4f194aa313e
celin <celin@Kapalangit>
2018-12-11 21:20:51 +0900

    Change title into image. Correct footsteps div.

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="./home.css" type="text/css">
-    <link rel="icon" href="./strawberry1.png">
+    <link rel="icon" href="./img/strawberry1.png">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Merriweather|Charmonman|Noto+Serif" rel="stylesheet">
     <title>Celine Yuwono</title>
@@ -14,10 +14,8 @@
 <body>  <!-- id indicates page; is used by menu CSS to indicate active page.  No JS needed. -->
     <span class="main-content" id="home"></span>
     <div class="title"><br/><br/>
-        <img src="./img/strawberry-moon-x.png" style="position:absolute; top:45px; left:47%; width:6vw; z-index:1;"/>
-        <!--<img src="./img/strawberry-moon-x.png" style="position:absolute; top:45px; left:47%; width:12vh; z-index:1;"/>-->
+        <img src="./img/title-image.png" id="center" style="position:absolute; width:500px; z-index:1;"/>
         <!-- <h2>Welcome to my blog</h2> -->
-        <h1>Celine &nbsp;Yuwono</h1>
     </div>
     <div class="menu" id="menu-scroll">
         <nav>
@@ -53,11 +51,17 @@
         <img class="warp-image" src="./img/lavender.jpg"/>
     </div>
     
-    <img src="./img/male-footprint-first.png" style="position:absolute; left:0.5%;width:5%;"/>
-    <img src="./img/male-footprint.png" style="position:absolute; left:0.5%; width:5%; top:148.5%;"/>
+    <div class="footprint-man-right">
+        <img src="./img/male-footprint-first.png"/>
+        <img src="./img/male-footprint.png"/>
+        <img src="./img/male-footprint.png"/>    
+    </div>
     
-    <img src="./img/female-footprint-first.png" style="position:absolute; right: 1.5%; width:3.5%;"/>
-    <img src="./img/female-footprint.png" style="position:absolute; right: 1.5%; width:3.5%; top:147.5%;"/>
+    <div class="footprint-woman-left">
+        <img src="./img/female-footprint-first.png"/>
+        <img src="./img/female-footprint.png"/>
+        <img src="./img/female-footprint.png"/>
+    </div>
     
     <main>
         <span class="anchor" id="about-me"></span>
@@ -95,12 +99,12 @@
     <img class="cloud-border" src="./img/cloud-border.png" width="99.99%";/>
     
     <footer>
-        <img src="./img/strawberry6.png" style="position: absolute;margin: -50px 0 0 5%;max-height: 7%; transform: rotate(20deg)">
-        <img src="./img/strawberry1.png" style="position: absolute; margin: -50px 0 0 22.6%; max-height: 7%; transform: rotate(20deg)">
-        <img src="./img/strawberry2.png" style="position: absolute; margin: -50px 0 0 41%; max-height: 7%;">
-        <img src="./img/strawberry4.png" style="position: absolute; margin: -50px 0 0 59%; max-height: 7%; transform: rotate(330deg);">
-        <img src="./img/strawberry5.png" style="position: absolute; margin: -50px 0 0 77.2%; max-height: 7%; transform: rotate(330deg);">
-        <img src="./img/strawberry3.png" style="position: absolute; margin: -50px 0 0 93%; max-height: 7%;">
+        <img src="./img/strawberry6.png" style="position: absolute;margin: -50px 0 0 5%;max-height: 60px; transform: rotate(20deg)">
+        <img src="./img/strawberry1.png" style="position: absolute; margin: -50px 0 0 22.6%; max-height: 60px; transform: rotate(20deg)">
+        <img src="./img/strawberry2.png" style="position: absolute; margin: -50px 0 0 41%; max-height: 60px;">
+        <img src="./img/strawberry4.png" style="position: absolute; margin: -50px 0 0 59%; max-height: 60px; transform: rotate(330deg);">
+        <img src="./img/strawberry5.png" style="position: absolute; margin: -50px 0 0 77.2%; max-height: 60px; transform: rotate(330deg);">
+        <img src="./img/strawberry3.png" style="position: absolute; margin: -50px 0 0 93%; max-height: 60px;">
         <br/><br/><br/><br/><br/><br/><br/><br/>
     </footer>
 </body>
