f0ba574dd12d90666763754467f5716d808fbec2
Celine Yuwono <celineyuwono@gmail.com>
2019-04-08 22:58:42 +0900

    Add languages.

---
+++ content/jp-contact.js
@@ -0,0 +1,38 @@
+document.write('\
+\   <span class="agile-red contact-title">ユヲノ・セリン</span><br/> \
+\   <span>エンジニア<span/><br/>\
+\   <i class="fas fa-envelope">&nbsp;</i>\
+\	メール: yuwono@agilemedia.jp<br/>\
+\   <i class="fas fa-phone">&nbsp;</i>\
+\	携帯電話: +81-80-8865-2696</span><br/><br/>\
+\	<span class="agile-red">アジャイルメディア・ネットワーク株式会社</span></br>\
+\	<span>東京都港区虎ノ門3-8-21<br/>\
+\   虎ノ門33森ビル<br/>\
+\   <span class="agile-red">https://agilemedia.jp</span>\
+\	</span>\
+\   <style>\
+\       .contact-title{\
+\           font-weight: bold;\
+\           font-style: italic;\
+\           font-size: 24px;\
+\           margin-left: -2px;\
+\       }\
+\       .agile-red{\
+\           color: rgb(177,0,44)\
+\        }\
+\   </style>\
+');
+
+
+/*rgb(177, 0, 44)
+document.write('\
+\   <p>Celine Yuwono<br/> \
+\   エンジニア<br/>\
+\	メール: yuwono@agilemedia.jp<br/>\
+\	携帯電話：+81-80-8865-2696<br/><br/>\
+\	アジャイルメディア・ネットワーク株式会社</br>\
+\	東京都港区虎ノ門3-8-21 虎ノ門33森ビル1階\
+\   https://agilemedia.jp\
+\	</p>\
+');
+*/
---
+++ content/zh-contact.js
@@ -0,0 +1,38 @@
+document.write('\
+\   <span class="agile-red contact-title">俞雪绫</span><br/> \
+\   <span>网络开发人员<span/><br/>\
+\   <i class="fas fa-envelope">&nbsp;</i>\
+\	电子邮件: yuwono@agilemedia.jp<br/>\
+\   <i class="fas fa-phone">&nbsp;</i>\
+\	电话号码: +81-80-8865-2696</span><br/><br/>\
+\	<span class="agile-red">Agile Media Network Co.Ltd</span></br>\
+\   <span>东京都港区虎门3-8-21<br/>\
+\   虎门33森建筑<br/>\
+\   <span class="agile-red">https://agilemedia.jp</span>\
+\	</span>\
+\   <style>\
+\       .contact-title{\
+\           font-weight: bold;\
+\           font-style: italic;\
+\           font-size: 24px;\
+\           margin-left: -2px;\
+\       }\
+\       .agile-red{\
+\           color: rgb(177,0,44)\
+\        }\
+\   </style>\
+');
+
+
+/*rgb(177, 0, 44)
+document.write('\
+\   <p>Celine Yuwono<br/> \
+\   エンジニア<br/>\
+\	メール: yuwono@agilemedia.jp<br/>\
+\	携帯電話：+81-80-8865-2696<br/><br/>\
+\	アジャイルメディア・ネットワーク株式会社</br>\
+\	東京都港区虎ノ門3-8-21 虎ノ門33森ビル1階\
+\   https://agilemedia.jp\
+\	</p>\
+');
+*/
